f3c476f05f5863f25d83293e1d25c28eddae323f
sebastiandig <42149171+sebastiandig@users.noreply.github.com>
2023-06-12 12:40:24 -0400

    miscellaneous functions to use

---
+++ scripts/misc_functions.R
@@ -0,0 +1,295 @@
+# ============================================================================ #
+# ---- Misc Functions ----
+# ============================================================================ #  
+
+
+get_sheet_info <- function(files) {
+  recal <- tibble()
+  
+  for (i in seq(nrow(files))) {
+    # select sheet name with `field_logsheet`
+    cli::cli_alert_info("Getting sheet info for file: {.file {files$base[i]}}")
+    temp_sht <- excel_sheets(files$file_path[i]) 
+    sht_num  <- which(str_detect(temp_sht, "field_"))
+    
+    if (is_empty(sht_num)) {
+      sht_num <- which(str_detect(temp_sht, "Sheet1")) 
+    } 
+    
+    if (is_empty(sht_num)) {
+      sht_num <- menu(temp_sht, 
+                      title = glue(
+                        "\n-----\n\n",
+                        "Which sheet contains metadata?", 
+                        "\n(0 for none)"))
+    }
+    
+    # skip if no sheet name
+    if (sht_num == 0) {
+      recal <- 
+        bind_rows(
+          recal,
+          tibble(file_path = files$file_path[i]))
+      next
+    }
+    
+    # read sheet 
+    temp <- read_excel(
+      files$file_path[i],
+      sheet        = sht_num, 
+      n_max        = 5,
+      col_names    = FALSE,
+      .name_repair = "unique_quiet") 
+    
+    # row number that contains headers
+    row <- which(apply(temp, 1, function(x) any(grepl("(?i)sample", x))))
+    
+    if (is_empty(row) || is.na(row)) {
+      View(temp)
+      row <- readline("Which line is the header? ") %>%
+        as.numeric()
+    }
+    
+    # skip if no row info
+    if (is_empty(row) || is.na(row)) {
+      recal <-
+        bind_rows(recal, 
+                  tibble(file_path    = files$file_path[i], 
+                         sht_num = sht_num))
+      next
+    }
+    
+    # get last column to read
+    # either `notes` or `collector`
+    last_c <- which(grepl("(?i)notes|(?i)collector", temp[row,])) %>%
+      max()
+    
+    recal <- 
+      bind_rows(
+        recal,
+        tibble(
+          file_path = files$file_path[i],
+          sht_num = sht_num,
+          row     = row,
+          last_c  = last_c
+        )
+      )
+  }
+  
+  # joins recal and files
+  files <- recal %>%
+    drop_na(everything()) %>%
+    left_join(files, by = "file_path")
+  
+  return(
+    list(files,
+         sht_num))
+  
+  # ---- end of function
+}
+
+
+##%######################################################%##
+#                                                          #
+####         Read Cruise Logsheets and Convert          ####
+#                                                          #
+##%######################################################%##
+#' Read Cruise Logsheets and Convert
+#'
+#' FUNCTION_DESCRIPTION
+#'
+#' @param .x File path
+#' @param .y Sheet name
+#' @param .z Rows to skip
+#' @param .l Values that should be treated as `NA`
+#' @param ship_acr The 2 letter ship acronym to match
+#'
+#' @return RETURN_DESCRIPTION
+#'
+#' @author Sebastian Di Geronimo (June 09, 2023)
+#'
+#' @examples
+#' # ADD_EXAMPLES_HERE
+#' 
+read_logsheets <-  function(.x, .y, .z, .l,
+                            ship_acr = "FK|WS|SV|WB|H") {
+  
+  # ---- load data
+  # select sheet, skip number of lines to sample 1, stop columns after
+  # `notes` or `collector`, remove NA values, clean names
+  temp <- read_xlsx(
+    path         = .x,
+    sheet        = .y,
+    skip         = .z - 1,
+    range        = cell_cols(c(NA, .l)),
+    .name_repair = janitor::make_clean_names,
+    na           = na_skip
+  ) %>%
+    
+    # ---- fix time
+    # time_gmt and time_sampled_24_00 to sample_collection_time_gmt
+    # fix sample_id to identifier
+    rename(any_of(
+      c(sample_collection_time_gmt = "time_gmt",
+        sample_collection_time_gmt = "sample_collection_time_hh_mm",
+        # TODO: need to correct to gmt ----
+        sample_collection_time_gmt = "sample_collection_time_edt", 
+        sample_collection_time_gmt = "time_sampled_24_00",
+        identifier = "sample_id")))  %>%
+    
+    # make sure the identifiers contain: FK, WS, SV or WB
+    filter(str_detect(identifier, ship_acr)) %>%
+    
+    # if vol_mol contains - or ~, remove 
+    mutate(
+      vol_ml    = replace(vol_ml,  str_detect(vol_ml, "-"), NA),
+      vol_ml    = str_replace_all(vol_ml, "~", "")
+    ) %>%
+    
+    # try to convert to `numeric`
+   suppressMessages(type_convert(.)) %>%
+    
+    # make sure notes are strings
+    mutate(
+      notes = as.character(notes)
+    ) 
+  
+  
+  # ---- fix issues related to few sheets 
+  # fix vol_ml if contains `&` to add two values
+  if (typeof(temp$vol_ml) == "character") {
+    temp <- temp %>%
+      mutate(
+        vol_ml = (str_split(vol_ml, " & ")),
+        vol_ml = map(vol_ml, ~ sum(as.numeric(.x)))) %>%
+      unnest(vol_ml) %>%
+      suppressMessages(type_convert(.))
+  }
+  
+  # fix depth_m to remove any `*` or `..`, then convert to num
+  if (typeof(temp$depth_m) == "character") {
+    temp <- temp %>%
+      mutate(
+        depth_m = str_remove(depth_m, "(\\*)+"),
+        depth_m = str_replace(depth_m, "\\.\\.", "\\.")
+      ) %>%
+      suppressMessages(type_convert(.))
+  }
+  
+  # fix max_depth to replace `flow through` to 0
+  if (typeof(temp$max_depth) == "character") {
+    temp <- mutate(temp,
+                   max_depth = str_replace(max_depth, "flo.*", "0")) %>%
+      suppressMessages(type_convert(.))
+  }
+  
+  # fix date_mm_dd_yy when excel date is non-numeric
+  # i.e. error 08//01/2015 - encoded as string, all other values are 
+  # read as strings (i.e. `"48025"` as string instead of `48025` as num)
+  if (typeof(temp$date_mm_dd_yy) == "character") {
+    temp <- temp %>%
+      mutate(
+        date_mm_dd_yy = str_replace(date_mm_dd_yy, "//", "/"),
+        date_mm_dd_yy = map_chr(date_mm_dd_yy,
+                                function(.x) {
+                                  if (!is.na(as.numeric(.x))) {
+                                    times <- as.numeric(.x) %>%
+                                      janitor::excel_numeric_to_date()
+                                  } else {
+                                    # convert strings "8/1/2015" to date 
+                                    times <- anytime::anydate(.x)
+                                  }
+                                  return(as.character(times))
+                                }),
+        
+        # converts to correct format
+        date_mm_dd_yy = suppressWarnings(as.POSIXct(date_mm_dd_yy))
+      )
+  }
+  
+  if (any(str_detect(names(temp), "sample_number" ))) {
+    temp <- 
+      mutate(temp, sample_number = as.character(sample_number))
+  }
+  
+  # add date_time, 
+  # first, converting excel date_time to HMS
+  # i.e. 1899-01-01 12:15:34 UTC to 12:15:13
+  # then, add date and time
+  temp <- mutate(
+    temp,
+    sample_collection_time_gmt = hms::as_hms(sample_collection_time_gmt),
+    date_time = date_mm_dd_yy + sample_collection_time_gmt,
+    .after = sample_collection_time_gmt) %>%
+    
+    # remover extract column if exists
+    select(-any_of("time_filtered_24_00")) %>%
+    
+    # fix date when entered as mm:dd:yy instead of mm/dd/yyyy
+    {if (nrow(filter(., date_mm_dd_yy > as_date("2015-01-01"))) < 1) {
+      mutate(., 
+             date_mm_dd_yy = as.character(date_mm_dd_yy), 
+             date_mm_dd_yy = str_replace(date_mm_dd_yy, ".* ", ""),
+             date_mm_dd_yy = str_replace_all(date_mm_dd_yy, ":", "/"),
+             date_mm_dd_yy = str_replace_all(date_mm_dd_yy, "(.*/.*/)", "\\120"),
+             date_mm_dd_yy = anytime::anydate(date_mm_dd_yy),
+             .before = 1)
+    } else {.}}
+  
+  return(temp)
+  
+  # ---- end of function
+}
+
+##%######################################################%##
+#                                                          #
+####         Search Cloud Directory for Folders         ####
+#                                                          #
+##%######################################################%##
+#' Search Cloud Directory for Folders
+#'
+#' FUNCTION_DESCRIPTION
+#'
+#' @param .cloud_dir Starting cloud directory
+#' @param .sub_folder Subfolders exists within every level
+#'                    Can be:
+#'                    - a string (ex `"dir1"`),
+#'                    - a vector (ex `c("dir1", "dir2")`), or 
+#'                    - a list (ex `list("dir1", "dir2")`)
+#'
+#' @return RETURN_DESCRIPTION
+#' @examples
+#' # ADD_EXAMPLES_HERE
+search_cloud_folders <- function(
+    .cloud_dir,
+    .sub_folder = NULL,
+    return_type = c("vector", "tibble")) {
+  
+  return_type <- match.arg(return_type)
+  
+  file_loc <- here::here(.cloud_dir) %>%
+    fs::dir_ls(type = "directory") %>%
+    fs::dir_ls(type = "directory")
+  
+  if (is.list(.sub_folder) || is.character(.sub_folder)) {
+    message("Using subfolder")
+    file_loc <- 
+    here::here(file_loc, as.list(.sub_folder)) %>%
+    Filter(fs::dir_exists, .)
+    
+  } else if (is.null(.sub_folder) || is.na(.sub_folder)) {
+    message("Not using subfolder")
+    file_loc <- Filter(fs::dir_exists, file_loc)  
+  } else {
+    stop("`.sub_folder` is not of type list, or character, or is NULL")
+  }
+  
+  switch(return_type,
+    vector = file_loc,
+    tibble = tibble::tibble(folder = file_loc) 
+  )
+  
+  
+
+  # ---- end of function
+}
